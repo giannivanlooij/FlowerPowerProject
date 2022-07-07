9a06c481e3b6c97c1b876242b8103cbc8736e13a
giannivanlooij <gvanlooij@roc-dev.com>
2022-07-07 12:52:00 +0200

    updated the dashboard

--- dashboard.php
+++ dashboard.php
@@ -112,7 +112,29 @@
        <!-- <i class="material-icons opacity-10">view_in_ar</i> -->
       </div>
     
-    <span class="nav-link-text ms-1">Voeg medewerkers toe</span>
+    <span class="nav-link-text ms-1">Voeg medewerker toe</span>
+  </a>
+</li>
+
+<li class="nav-item">
+  <a class="nav-link text-white " href="pages/employees/create-employee.php">
+    
+      <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+       <!-- <i class="material-icons opacity-10">view_in_ar</i> -->
+      </div>
+    
+    <span class="nav-link-text ms-1">Klanten</span>
+  </a>
+</li>
+
+<li class="nav-item">
+  <a class="nav-link text-white " href="pages/employees/create-employee.php">
+    
+      <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+       <!-- <i class="material-icons opacity-10">view_in_ar</i> -->
+      </div>
+    
+    <span class="nav-link-text ms-1">Voeg Klant toe</span>
   </a>
 </li>
 
