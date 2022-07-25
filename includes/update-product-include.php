54954f8c9c905925b3bf37c08bb5eb97694e978f
giannivanlooij <gvanlooij@roc-dev.com>
2022-07-25 20:13:03 +0200

    made a empty ifstatement to counter the problem

--- includes/update-product-include.php
+++ includes/update-product-include.php
@@ -7,7 +7,25 @@
         $Product_Description = $_POST['Product_Description'];
         $Product_Price = $_POST['Product_Price'];
         $Product_Stock = $_POST['Product_Stock'];
-        $Product_ImgLocation = $_POST['Product_ImgLocation'];
+        $Product_Image = $_FILES["Product_Image"];
+
+        
+        
+        if (!empty($Product_Image)) {
+            $Product_Image = $_FILES["Product_Image"];
+            $Temp_Location = $_FILES["Product_Image"] ["tmp_name"];
+            $Target_Location = "../Images/productimages/" . time(). $_FILES["Product_Image"] ["name"];
+
+            move_uploaded_file($Temp_Location, $Target_Location);
+            
+        } else {
+            echo "no picture set";
+        }
+
+
+
+
+        
         
 
 
@@ -16,7 +34,7 @@
         `Product_Description`='$Product_Description',
         `Product_Price`='$Product_Price',
         `Product_Stock`='$Product_Stock',
-        `Product_ImgLocation`='$Product_ImgLocation'
+        `Product_ImgLocation`='$Target_Location'
         WHERE Product_ID = $Product_ID";
 
         if ($conn->query($sql) === TRUE) {
