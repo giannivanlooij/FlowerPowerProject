5289fefd687c6362181a580e96c7cab7530fa54b
giannivanlooij <gvanlooij@roc-dev.com>
2022-08-20 12:45:03 +0200

    one product can be added to the invoice
    still looking for a way to do multiple

--- includes/shoppingcart-handler.php
+++ includes/shoppingcart-handler.php
@@ -2,7 +2,7 @@
 session_start();
 include_once "databasehandler-include.php";
 
-//prepares customer login to insert his data into the database
+//prepares the arrays
 if (isset($_SESSION['Customer_ID'])) {
 
     $ID = $_SESSION['Customer_ID'];
@@ -10,50 +10,67 @@ if (isset($_SESSION['Customer_ID'])) {
     $PickedUp = 0;
     $Date = date("Y-m-d");
 
-    echo $ID;
-    echo $Name;
+    //$product_ID = $values['Product_ID'];
+    //$Product_Quantity = $values['Product_Quantity'];
 }
 
 // adds the products to the productsoninvoice
 if($_GET["action"] == 'checkout')
     {
+        //prepares the invoice 
         $InvoiceQuery = "INSERT INTO invoices (Customer_ID, Invoice_Date, Invoice_OrderPickedUp) VALUES (?, ?, ?);";
         $InvoiceStatement = mysqli_stmt_init($conn);
         if (!mysqli_stmt_prepare($InvoiceStatement, $InvoiceQuery)) {
         header("location: ../index.php?error=stmt2failed");
         Exit();
         }
- 
         mysqli_stmt_bind_param($InvoiceStatement, "sss", $ID, $Date, $PickedUp);
         mysqli_stmt_execute($InvoiceStatement);
         mysqli_stmt_close($InvoiceStatement);
-        echo $Date;
-        //header("location: ../index.php?error=none");
-        Exit();
+        echo "Invoice created";
 
-        echo "check database for invoice";
 
+        //fetches the id from the just generated invoice
+        $FetchID = "SELECT MAX(Invoice_ID) FROM invoices;";
+        $ResultID = mysqli_query($conn, $FetchID);
+        $ResultIDCheck = mysqli_num_rows($ResultID);
+
+        
+        
+
+        if ($ResultIDCheck > 0) {
+            while ($row = $ResultID->fetch_row()) {
+                $GeneratedID = $row[0] ?? false;
+                echo $GeneratedID;
+
+                foreach($_SESSION["AddedToCart"] as $Keys => $values){
+        
+                    // die('so far so good');
+                    $ProductQuery = "INSERT INTO productsoninvoices (Invoice_ID, Product_ID, ItemOnInvoice_Quantity) VALUES (?, ?, ?);";
+                    $ProductStatement = mysqli_stmt_init($conn);
+                    if (!mysqli_stmt_prepare($ProductStatement, $ProductQuery)) {
+                    header("location: ../index.php?error=stmt2failed");
+                    Exit();
+                        }
+                        mysqli_stmt_bind_param($ProductStatement, "sss", $GeneratedID, $values['Product_ID'], $values['Product_Quantity']);
+                        mysqli_stmt_execute($ProductStatement);
+                        mysqli_stmt_close($ProductStatement);
+                        echo "productsoninvoices created";
+            
+                    //header("location: ../index.php?error=none");
+                    Exit();
+                    }
+            }
+
+            // adds the products to the productsoninvoice
+        echo "session is set for adding items and foreach";
+        
+    }
     }
 
 
-// adds the products to the productsoninvoice
-if($_GET["action"] == 'checkout')
-    {
-        if(isset($_SESSION["AddedToCart"]))
-        {
-            foreach($_SESSION["AddedToCart"] as $values)
-        {
-            echo "sql is defined";
-             $sql ="INSERT INTO productsoninvoices (Product_ID, ItemOnInvoice_Quantity)
-                    values ('{$values['Product_ID']}', '{$values['Product_Quantity']}')";
-                    $statement = $conn->prepare($sql);
-                    $statement->execute();
 
-                    echo "check database for products";
 
-        }
-    }
-}
 
 ?>
 
