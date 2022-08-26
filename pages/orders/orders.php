5ebe061b7f967824119e86a7c0edf95e2cc275af
giannivanlooij <gvanlooij@roc-dev.com>
2022-08-26 11:22:51 +0200

    location on order works but result is not good
    the location is succesfully in the result. only the employees do not match

--- pages/orders/orders.php
+++ pages/orders/orders.php
@@ -6,18 +6,36 @@
             <th scope="col">Datum</th>
             <th scope="col">Klant</th>
             <th scope="col">vestiging</th>
+            <th scope="col">Werknemer</th>
             <th class="text-center" scope="col">opgehaald</th>
         </tr>
     </thead>
     <tbody>
         <?php
+
+            $FetchWorkLocation = "SELECT Employee_WorksAt from Employees Where Employee_ID = $Employee_ID";
+            $FetchResult = mysqli_query($conn, $FetchWorkLocation);
+            $FetchResultCheck = mysqli_num_rows($FetchResult);
+
+            if ($FetchResultCheck > 0) {
+                while ($Row = mysqli_fetch_assoc($FetchResult)) {
+                    //defined variables
+                    $Employee_WorksAt = $Row['Employee_WorksAt'];
+                }
+            }
+                    
+            
+
         
-            $sql = "SELECT Invoice_ID, Invoice_OrderPickedUp, Invoice_Date, Customer_Name, FlowerShop_Addres
+            $sql = "SELECT Invoice_ID, Invoice_OrderPickedUp, Invoice_Date, Customer_Name, Employee_Name, FlowerShop_Addres
             FROM invoices
             INNER JOIN customers
             ON invoices.Customer_ID = customers.Customer_ID
             INNER JOIN flowershops
-            ON invoices.FlowerShop_ID = flowershops.FlowerShop_ID";
+            ON invoices.FlowerShop_ID = flowershops.FlowerShop_ID
+            INNER JOIN employees
+            ON flowershops.FlowerShop_ID = employees.Employee_WorksAt
+            Where employees.Employee_WorksAt = $Employee_WorksAt";
 
 
             $Result = mysqli_query($conn, $sql);
@@ -30,6 +48,7 @@
                     $OrderDate = $Row['Invoice_Date'];
                     $Customer_Name = $Row['Customer_Name'];
                     $FlowerShop = $Row['FlowerShop_Addres'];
+                    $Employee_Name = $Row['Employee_Name'];
                     $OrderPickedUp = $Row['Invoice_OrderPickedUp'];
 
                     if ($OrderPickedUp == 1) {
@@ -65,7 +84,12 @@
                         $FlowerShop .
                         "</div>" .
                     "</td>" .
-                    
+                    //Employee assigned
+                    "<td>" .
+                        "<div class='r-no'>" .
+                        $Employee_Name .
+                        "</div>" .
+                    "</td>" .
                     //order picked up
                     "<td>" .
                         "<div class='r-no'>" .
