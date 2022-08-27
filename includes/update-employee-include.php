4d1a0cd90c462616559fc3a98fc90c48f0a32c54
giannivanlooij <gvanlooij@roc-dev.com>
2022-08-27 11:42:52 +0200

    password hash on update employee

--- includes/update-employee-include.php
+++ includes/update-employee-include.php
@@ -16,6 +16,8 @@
         $Employee_DateOfBirth = $_POST['Employee_DateOfBirth'];
         $FlowerShop_ID = $_POST['FlowerShop_ID'];
 
+        $HashedPassword = password_hash($Employee_Password, PASSWORD_DEFAULT);
+
 
         $sql = "UPDATE `employees` SET 
         `Employee_Name`='$Employee_Name',
@@ -26,7 +28,7 @@
         `Employee_PostalCode`='$Employee_PostalCode',
         `Employee_TownShip`='$Employee_TownShip',
         `Employee_Email`='$Employee_Email',
-        `Employee_Password`='$Employee_Password',
+        `Employee_Password`='$HashedPassword',
         `Employee_PhoneNumber`='$Employee_PhoneNumber',
         `Employee_DateOfBirth`='$Employee_DateOfBirth',
         `Employee_WorksAt`='$FlowerShop_ID' 
